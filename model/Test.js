2169e49a99a87d70aaa99e507ed2da24951f82bf
adamsyy <adamoommenjacob@gmail.com>
2022-05-26 01:54:54 +0530

    check

--- model/Test.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const Test = new Schema({
-  name: String,
-});
-
-
-const UserSchema = mongoose.model("Test", Test);
-module.exports = UserSchema;
